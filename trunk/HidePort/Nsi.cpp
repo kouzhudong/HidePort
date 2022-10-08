c07015634f31f3ecdfe85e4d89a72f1af9e8f217
correy <112426112@qq.com>
2022-10-08 10:09:31 +0800

    改进。

--- trunk/HidePort/Nsi.cpp
+++ trunk/HidePort/Nsi.cpp
@@ -4,6 +4,32 @@
 //////////////////////////////////////////////////////////////////////////////////////////////////
 
 
+NTSTATUS MyNsippEnumerateObjectsAllParameters(PVOID InputBuffer,
+                                              SIZE_T Length,
+                                              KPROCESSOR_MODE RequestorMode,
+                                              PULONG_PTR Information
+)
+/*
+功能：隐藏端口。
+
+做法：摘除需要指定摘除的信息。
+
+重点：InputBuffer的结构类型。
+
+篡改时机：IoCallDriver之后。
+*/
+{
+    NTSTATUS Status = STATUS_SUCCESS;
+
+    UNREFERENCED_PARAMETER(InputBuffer);
+    UNREFERENCED_PARAMETER(Length);
+    UNREFERENCED_PARAMETER(RequestorMode);
+    UNREFERENCED_PARAMETER(Information);
+
+    return Status;
+}
+
+
 NTSTATUS NsiDeviceControl(_In_ PIO_STACK_LOCATION IrpStack, _Inout_ PIRP Irp)
 /*
 做法参考：nsiproxy!NsippDispatch.
@@ -13,23 +39,27 @@ NTSTATUS NsiDeviceControl(_In_ PIO_STACK_LOCATION IrpStack, _Inout_ PIRP Irp)
 {
     NTSTATUS Status = STATUS_SUCCESS;
     PIO_STACK_LOCATION  irpSp;// Pointer to current stack location
-    ULONG               inBufLength; // Input buffer length
+    ULONG               InputBufferLength; // Input buffer length
     ULONG               outBufLength; // Output buffer length
     PCHAR               inBuf, outBuf; // pointer to Input and output buffer
 
     UNREFERENCED_PARAMETER(IrpStack);
 
     irpSp = IoGetCurrentIrpStackLocation(Irp);
-    inBufLength = irpSp->Parameters.DeviceIoControl.InputBufferLength;
+    InputBufferLength = irpSp->Parameters.DeviceIoControl.InputBufferLength;
     outBufLength = irpSp->Parameters.DeviceIoControl.OutputBufferLength;
-    //PVOID Type3InputBuffer = irpSp->Parameters.DeviceIoControl.Type3InputBuffer;
+    PVOID Type3InputBuffer = irpSp->Parameters.DeviceIoControl.Type3InputBuffer;
     ULONG IoControlCode = irpSp->Parameters.DeviceIoControl.IoControlCode;
     inBuf = (PCHAR)Irp->AssociatedIrp.SystemBuffer;
     outBuf = (PCHAR)Irp->AssociatedIrp.SystemBuffer;
+    KPROCESSOR_MODE RequestorMode = Irp->RequestorMode;
 
     switch (IoControlCode) {
     case 0x12001Bu://NsippEnumerateObjectsAllParameters
-
+        MyNsippEnumerateObjectsAllParameters(Type3InputBuffer,
+                                             (SIZE_T)InputBufferLength,
+                                             RequestorMode,
+                                             &Irp->IoStatus.Information);
         break;
     case 0x120007u://NsippGetParameter
 
